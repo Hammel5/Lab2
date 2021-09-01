c6dfc32ad914e5bb9a5985f54d6ec4e9dce3b8bd
HAMMEL5 <HAMMEL5@IC-024852791151>
2021-09-01 16:28:35 -0500

    Main Schedule Program

---
+++ Lab2/Main.cpp
@@ -0,0 +1,36 @@
+/*
+	Dalton Hammel
+	C++ Fall 2021
+	Due: 9/15/2021
+	Lab 2 Exploring Output
+	This lab is designed to get you familiar output to the command prompt using C++.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Output Schedule to the Screen
+	const string m = "Monday";
+	const string tu = "Tuesday";
+	const string w = "Wednesday";
+	const string th = "Thursday";
+	const string f = "Friday";
+	const string fct = "8:00";
+	const string sct = "8:20";
+	const string tct = "10:00";
+	const string fcn = "American National Government"; 
+	const string scn = "Python Programming";
+	const string tcn = "C++";
+	cout << m << "\t" << "\t" << fct << "\t" << fcn << endl;
+	cout << m << "\t" << "\t" << tct << "\t" << tcn << endl;
+	cout << tu << "\t" << "\t" << sct << "\t" << scn << endl;
+	cout << tu << "\t" << "\t" << tct << "\t" << tcn << endl;
+	cout << w << "\t" << fct << "\t" << fcn << endl;
+	cout << w << "\t" << tct << "\t" << tcn << endl;
+	cout << th << "\t" << sct << "\t" << scn << endl;
+	cout << th << "\t" << tct << "\t" << tcn << endl;
+	cout << f << "\t" << "\t" << tct << "\t" << tcn << endl;
+
+}
